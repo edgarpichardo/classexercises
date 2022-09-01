838d8b3826398c136386da390ccc734a211157e6
edgarpichardo <ehp87321@uga.edu>
2022-08-31 20:37:54 -0400

    commandline

---
+++ commandline/part1.sh
@@ -0,0 +1,14 @@
+mkdir part1
+cd part1
+touch earth.txt mars.txt neptune.txt uranus.txt jupiter.txt mercury.txt saturn.txt venus.txt
+mkdir planets 
+mv *.txt planets 
+mv planets solarsystem 
+pwd
+ls -pal 
+cd solarsystem 
+pwd
+ls 
+cd ..
+pwd 
+
---
+++ commandline/part2.sh
@@ -0,0 +1,23 @@
+mkdir part2
+cd part2
+cd ..
+cp -r part1/solarsystem part2/solarsystem
+cd part2
+cd solarsystem
+mkdir planets
+mv *.txt planets
+cp -r planets planets_save
+pwd
+ls 
+cd planets
+pwd
+ls
+cd ..
+cd planets_save
+pwd
+ls
+cd ..
+rm -rf planets_save
+pwd
+ls
+cd .. 
